a4c3107efde0541d0f070bf16229fe7c5f38787b
wtlgo <georgeendo0@gmail.com>
2023-02-28 18:03:32 +0300

    Fix permissions

--- src/main/java/dev/mikchan/mcnp/chat/commands/command/ReloadCommand.kt
+++ src/main/java/dev/mikchan/mcnp/chat/commands/command/ReloadCommand.kt
@@ -4,12 +4,13 @@ import dev.mikchan.mcnp.chat.ChatPlugin
 import net.md_5.bungee.api.ChatColor
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
+import org.bukkit.command.ConsoleCommandSender
 
 internal class ReloadCommand(private val plugin: ChatPlugin) : ICommand {
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         if (args.size != 1) return false
         if (!"reload".equals(args[0], true)) return false
-        if (sender.hasPermission("mcn.chat.admin.reload")) {
+        if (sender !is ConsoleCommandSender && !sender.hasPermission("mcn.chat.admin.reload")) {
             sender.sendMessage("${ChatColor.DARK_RED}You have no permission to do that.")
             return false
         }
