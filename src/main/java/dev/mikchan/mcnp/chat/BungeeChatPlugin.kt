09b15aeea99e393c6af77e122beb1a77f89ef1c5
wtlgo <georgeendo0@gmail.com>
2023-03-03 21:03:40 +0300

    Add missing documentation

--- src/main/java/dev/mikchan/mcnp/chat/BungeeChatPlugin.kt
+++ src/main/java/dev/mikchan/mcnp/chat/BungeeChatPlugin.kt
@@ -2,6 +2,9 @@ package dev.mikchan.mcnp.chat
 
 import net.md_5.bungee.api.plugin.Plugin
 
+/**
+ * The main bungee class
+ */
 @Suppress("unused", "MemberVisibilityCanBePrivate")
 class BungeeChatPlugin : Plugin() {
     override fun onEnable() {
