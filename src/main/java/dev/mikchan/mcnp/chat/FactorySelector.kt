faf050dac76cc81e405220b2a6f858918f9e6b94
wtlgo <georgeendo0@gmail.com>
2023-03-03 19:40:06 +0300

    Make FactorySelector internal

--- src/main/java/dev/mikchan/mcnp/chat/FactorySelector.kt
+++ src/main/java/dev/mikchan/mcnp/chat/FactorySelector.kt
@@ -3,7 +3,7 @@ package dev.mikchan.mcnp.chat
 import dev.mikchan.mcnp.chat.contract.IChatPluginFactory
 import dev.mikchan.mcnp.chat.implementation.spigot.SpigotChatPluginFactory
 
-object FactorySelector {
+internal object FactorySelector {
     fun selectFactory(plugin: ChatPlugin): IChatPluginFactory {
         return SpigotChatPluginFactory(plugin)
     }
