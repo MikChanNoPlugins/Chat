46751586a44ef0db714ba4a7df07f43f0f2d1aee
wtlgo <georgeendo0@gmail.com>
2023-03-03 10:39:34 +0300

    Add documentation

--- src/main/java/dev/mikchan/mcnp/chat/contract/IChatPluginFactory.kt
+++ src/main/java/dev/mikchan/mcnp/chat/contract/IChatPluginFactory.kt
@@ -12,12 +12,52 @@ import dev.mikchan.mcnp.chat.contract.users.IUserManager
  * An abstract factory
  */
 interface IChatPluginFactory {
+    /**
+     * Creates a configuration
+     *
+     * @return A new configuration object
+     */
     fun createConfig(): IConfig
+
+    /**
+     * Creates a command manager
+     *
+     * @return A new command manager
+     */
     fun createCommandManager(): ICommandManager
+
+    /**
+     * Creates an event manager
+     *
+     * @return A new event manager
+     */
     fun createEventManager(): IEventManager
+
+    /**
+     * Creates a formatter
+     *
+     * @return A new formatter
+     */
     fun createFormatter(): IFormatter
+
+    /**
+     * Creates a key store
+     *
+     * @return A new key store
+     */
     fun createKeys(): IKeys
+
+    /**
+     * Creates a user manager
+     *
+     * @return A new user manager
+     */
     fun createUserManager(): IUserManager
 
+    /**
+     * Creates a chat logger
+     *
+     * @return A new chat logger
+     */
     fun createChatLogger(): IChatLogger
 }
--- src/main/java/dev/mikchan/mcnp/chat/contract/keys/IKey.kt
+++ src/main/java/dev/mikchan/mcnp/chat/contract/keys/IKey.kt
@@ -3,7 +3,17 @@ package dev.mikchan.mcnp.chat.contract.keys
 import org.bukkit.NamespacedKey
 import org.bukkit.persistence.PersistentDataType
 
+/**
+ * Key representation
+ */
 interface IKey<T> {
+    /**
+     * The key name
+     */
     val key: NamespacedKey
+
+    /**
+     * The key type
+     */
     val type: PersistentDataType<T, T>
 }
