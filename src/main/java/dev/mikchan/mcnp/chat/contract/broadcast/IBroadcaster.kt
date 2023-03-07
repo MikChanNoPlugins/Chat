e02a9bce8cd1d929d0b5e44d2c518e6ef4bbf4dc
wtlgo <georgeendo0@gmail.com>
2023-03-08 02:23:55 +0300

    Forgot to commit this file...

--- src/main/java/dev/mikchan/mcnp/chat/contract/broadcast/IBroadcaster.kt
+++ src/main/java/dev/mikchan/mcnp/chat/contract/broadcast/IBroadcaster.kt
@@ -1,5 +1,7 @@
 package dev.mikchan.mcnp.chat.contract.broadcast
 
+import org.bukkit.entity.Player
+
 /**
  * A broadcaster interface, primarily intended for plugin-bungee interaction
  */
@@ -13,4 +15,12 @@ interface IBroadcaster {
      * Disables the broadcaster
      */
     fun disable()
+
+    /**
+     * Broadcasts global message
+     *
+     * @param sender The player who sends the message
+     * @param message Text of the message
+     */
+    fun broadcastGlobal(sender: Player, message: String)
 }
