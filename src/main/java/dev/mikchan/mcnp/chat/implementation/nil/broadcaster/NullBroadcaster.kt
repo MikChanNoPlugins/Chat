a478c673645e29ebbc03a45c44a601678d6c3441
wtlgo <georgeendo0@gmail.com>
2023-03-08 02:21:42 +0300

    Try to implement broadcastGlobal

--- src/main/java/dev/mikchan/mcnp/chat/implementation/nil/broadcaster/NullBroadcaster.kt
+++ src/main/java/dev/mikchan/mcnp/chat/implementation/nil/broadcaster/NullBroadcaster.kt
@@ -1,9 +1,12 @@
 package dev.mikchan.mcnp.chat.implementation.nil.broadcaster
 
 import dev.mikchan.mcnp.chat.contract.broadcast.IBroadcaster
+import org.bukkit.entity.Player
 
 internal class NullBroadcaster : IBroadcaster {
     override fun enable() {}
 
     override fun disable() {}
+
+    override fun broadcastGlobal(sender: Player, message: String) {}
 }
--- src/main/java/dev/mikchan/mcnp/chat/implementation/spigot/v1_16_5/broadcaster/SpigotV1m16p5Broadcaster.kt
+++ src/main/java/dev/mikchan/mcnp/chat/implementation/spigot/v1_16_5/broadcaster/SpigotV1m16p5Broadcaster.kt
@@ -1,5 +1,6 @@
 package dev.mikchan.mcnp.chat.implementation.spigot.v1_16_5.broadcaster
 
+import com.google.common.io.ByteStreams
 import dev.mikchan.mcnp.chat.ChatPlugin
 import dev.mikchan.mcnp.chat.contract.broadcast.IBroadcaster
 import org.bukkit.entity.Player
@@ -18,6 +19,16 @@ internal class SpigotV1m16p5Broadcaster(private val plugin: ChatPlugin) : IBroad
     override fun disable() {
         plugin.server.messenger.unregisterOutgoingPluginChannel(plugin, bungeeChannel)
         plugin.server.messenger.unregisterIncomingPluginChannel(plugin, bungeeChannel)
+    }
+
+    override fun broadcastGlobal(sender: Player, message: String) {
+        val formattedMsg = plugin.formatter.formatGlobal(sender, message)
+
+        val out = ByteStreams.newDataOutput()
+
+        out.writeUTF("Message")
+        out.writeUTF("ALL")
+        out.writeUTF(formattedMsg)
     }
 
     override fun onPluginMessageReceived(channel: String, player: Player, message: ByteArray) {
