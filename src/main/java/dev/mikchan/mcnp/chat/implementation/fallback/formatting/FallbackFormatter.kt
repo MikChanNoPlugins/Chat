7028097c634f0b0529d02d5a0171517ee6873282
wtlgo <georgeendo0@gmail.com>
2023-03-08 04:07:04 +0300

    Try to reduce duplication

--- src/main/java/dev/mikchan/mcnp/chat/implementation/fallback/formatting/FallbackFormatter.kt
+++ src/main/java/dev/mikchan/mcnp/chat/implementation/fallback/formatting/FallbackFormatter.kt
@@ -6,6 +6,31 @@ import org.bukkit.ChatColor
 import org.bukkit.entity.Player
 
 internal class FallbackFormatter(private val plugin: ChatPlugin) : BaseFormatter(plugin) {
+    private fun formatOnePlayer(
+        player: Player,
+        playerName: String,
+        template: String,
+        message: String,
+        alwaysColors: Boolean = false,
+    ): String {
+        return prepareTemplate(template,
+            { playerName },
+            { playerName },
+            { playerName },
+            { playerName },
+            { playerName },
+            { playerName },
+            {
+                if (alwaysColors || player.hasPermission("mcn.chat.colors")) {
+                    ChatColor.translateAlternateColorCodes(
+                        '&', message
+                    )
+                } else {
+                    ChatColor.stripColor(message)
+                }
+            })
+    }
+
     override fun formatPrivate(from: Player, to: Player, message: String): String {
         return prepareTemplate(plugin.config.privateTemplate,
             { "%1\$s" },
@@ -15,84 +40,33 @@ internal class FallbackFormatter(private val plugin: ChatPlugin) : BaseFormatter
             { "%1\$s" },
             { "%1\$s" },
             {
-                if (from.hasPermission("mcn.chat.colors")) ChatColor.translateAlternateColorCodes(
-                    '&', message
-                ) else ChatColor.stripColor(message)
+                if (from.hasPermission("mcn.chat.colors")) {
+                    ChatColor.translateAlternateColorCodes(
+                        '&', message
+                    )
+                } else {
+                    ChatColor.stripColor(message)
+                }
             })
     }
 
     override fun formatConsole(to: Player, message: String): String {
-        return prepareTemplate(plugin.config.consoleTemplate,
-            { to.displayName },
-            { to.displayName },
-            { to.displayName },
-            { to.displayName },
-            { to.displayName },
-            { to.displayName },
-            {
-                ChatColor.translateAlternateColorCodes(
-                    '&', message
-                )
-            })
+        return formatOnePlayer(to, to.displayName, plugin.config.consoleTemplate, message, false)
     }
 
     override fun formatToConsole(from: Player, message: String): String {
-        return prepareTemplate(plugin.config.consoleTemplate,
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            {
-                ChatColor.translateAlternateColorCodes(
-                    '&', message
-                )
-            })
+        return formatOnePlayer(from, "%1\$s", plugin.config.fromConsoleTemplate, message)
     }
 
     override fun formatGlobal(from: Player, message: String): String {
-        return prepareTemplate(plugin.config.globalTemplate,
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            {
-                if (from.hasPermission("mcn.chat.colors")) ChatColor.translateAlternateColorCodes(
-                    '&', message
-                ) else ChatColor.stripColor(message)
-            })
+        return formatOnePlayer(from, "%1\$s", plugin.config.globalTemplate, message)
     }
 
     override fun formatLocal(from: Player, message: String): String {
-        return prepareTemplate(plugin.config.localTemplate,
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            {
-                if (from.hasPermission("mcn.chat.colors")) ChatColor.translateAlternateColorCodes(
-                    '&', message
-                ) else ChatColor.stripColor(message)
-            })
+        return formatOnePlayer(from, "%1\$s", plugin.config.localTemplate, message)
     }
 
     override fun formatSpy(from: Player, message: String): String {
-        return prepareTemplate(plugin.config.spyTemplate,
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            { "%1\$s" },
-            {
-                if (from.hasPermission("mcn.chat.colors")) ChatColor.translateAlternateColorCodes(
-                    '&', message
-                ) else ChatColor.stripColor(message)
-            })
+        return formatOnePlayer(from, "%1\$s", plugin.config.spyTemplate, message)
     }
 }
