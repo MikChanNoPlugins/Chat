18de9ee450bef2591ba88811868ff80045ebf265
wtlgo <georgeendo0@gmail.com>
2023-06-22 18:16:37 +0300

    Add ignore command

--- src/main/java/dev/mikchan/mcnp/chat/implementation/spigot/v1_16_5/utility/SpigotV1m16p5Utility.kt
+++ src/main/java/dev/mikchan/mcnp/chat/implementation/spigot/v1_16_5/utility/SpigotV1m16p5Utility.kt
@@ -1,6 +1,22 @@
 package dev.mikchan.mcnp.chat.implementation.spigot.v1_16_5.utility
 
 import dev.mikchan.mcnp.chat.contract.utility.IUtility
+import java.util.*
 
 internal class SpigotV1m16p5Utility : IUtility {
+    override fun uniqueIdSetToByteArray(uuids: Set<UUID>): ByteArray {
+        TODO("Not yet implemented")
+    }
+
+    override fun byteArrayToUniqueIdSet(byteArray: ByteArray): Set<UUID> {
+        TODO("Not yet implemented")
+    }
+
+    override fun uniqueIdToByteArray(uuid: UUID): ByteArray {
+        TODO("Not yet implemented")
+    }
+
+    override fun byteArrayToUniqueId(byteArray: ByteArray): UUID? {
+        TODO("Not yet implemented")
+    }
 }
