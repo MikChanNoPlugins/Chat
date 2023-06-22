144bbc5844eb9d39f782a840e79ca35476d89653
wtlgo <georgeendo0@gmail.com>
2023-06-22 18:29:59 +0300

    Implement SpigotV1m16p5Utility

--- src/main/java/dev/mikchan/mcnp/chat/implementation/spigot/v1_16_5/utility/SpigotV1m16p5Utility.kt
+++ src/main/java/dev/mikchan/mcnp/chat/implementation/spigot/v1_16_5/utility/SpigotV1m16p5Utility.kt
@@ -1,22 +1,30 @@
 package dev.mikchan.mcnp.chat.implementation.spigot.v1_16_5.utility
 
 import dev.mikchan.mcnp.chat.contract.utility.IUtility
+import java.nio.ByteBuffer
 import java.util.*
 
 internal class SpigotV1m16p5Utility : IUtility {
     override fun uniqueIdSetToByteArray(uuids: Set<UUID>): ByteArray {
-        TODO("Not yet implemented")
+        return uuids.flatMap { uniqueIdToByteArray(it).toList() }.toByteArray()
     }
 
     override fun byteArrayToUniqueIdSet(byteArray: ByteArray): Set<UUID> {
-        TODO("Not yet implemented")
+        return byteArray.toList().chunked(16).mapNotNull { byteArrayToUniqueId(it.toByteArray()) }.toSet()
     }
 
     override fun uniqueIdToByteArray(uuid: UUID): ByteArray {
-        TODO("Not yet implemented")
+        val buffer = ByteBuffer.wrap(ByteArray(16))
+        buffer.putLong(uuid.mostSignificantBits)
+        buffer.putLong(uuid.leastSignificantBits)
+        return buffer.array()
     }
 
     override fun byteArrayToUniqueId(byteArray: ByteArray): UUID? {
-        TODO("Not yet implemented")
+        if (byteArray.size != 16) return null
+        val buffer = ByteBuffer.wrap(byteArray)
+        val high = buffer.getLong()
+        val low = buffer.getLong()
+        return UUID(high, low)
     }
 }
