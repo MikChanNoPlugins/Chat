536dd321ccd03c13e3627b3d18f8cb89543f2781
wtlgo <georgeendo0@gmail.com>
2023-06-23 10:46:29 +0300

    Add missing documentation

--- src/main/java/dev/mikchan/mcnp/chat/contract/utility/IUtility.kt
+++ src/main/java/dev/mikchan/mcnp/chat/contract/utility/IUtility.kt
@@ -6,9 +6,35 @@ import java.util.*
  * A Utility interface
  */
 interface IUtility {
+    /**
+     * Converts an [UUID] set to [ByteArray]
+     *
+     * @param uuids The set with UUIDs
+     * @return The resulting [ByteArray]
+     */
     fun uniqueIdSetToByteArray(uuids: Set<UUID>): ByteArray
+
+    /**
+     * Converts [ByteArray] to [UUID] set
+     *
+     * @param byteArray The [ByteArray]
+     * @return The resulting set
+     */
     fun byteArrayToUniqueIdSet(byteArray: ByteArray): Set<UUID>
 
+    /**
+     * Converts an [UUID] object to [ByteArray]
+     *
+     * @param uuid The [UUID] object
+     * @return The resulting [ByteArray]
+     */
     fun uniqueIdToByteArray(uuid: UUID): ByteArray
+
+    /**
+     * Converts a [ByteArray] to [UUID] object
+     *
+     * @param byteArray The [ByteArray]
+     * @return The resulting [UUID] object. If the [byteArray] is malformed, returns `null`.
+     */
     fun byteArrayToUniqueId(byteArray: ByteArray): UUID?
 }
